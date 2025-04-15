87679b88d182010d51b15f64a18578e3da1380fe
Dmitry-J89 <lblb8358@gmail.com>
2025-04-15 11:39:54 +0500

    ДЗ 15.04.25

--- homework/IfElseStatementTheme.java
+++ homework/IfElseStatementTheme.java
@@ -1,8 +1,8 @@
 public class IfElseStatementTheme {
     public static void main(String[] args) {
 
-        // 1 ЗАДАНИЕ
-        System.out.println("1 ПЕРЕВОД ПСЕВДОКОДА НА ЯЗЫК JAVA");
+        // 1 Задание
+        System.out.println("\n1. Перевод псевдокода на язык JAVA\n");
 
         boolean isMale = false;
         if (!isMale) {
@@ -25,54 +25,48 @@ public class IfElseStatementTheme {
             System.out.println("Рост: 1.8 м или выше");
         }
 
-        System.out.println();
+        // 2 Задание
+        System.out.println("\n2. Поиск большего числа шагов\n");
 
-        // 2 ЗАДАНИЕ
-        System.out.println("2 ПОИСК БОЛЬШЕГО ЧИСЛА ШАГОВ");
-
-        int stepsYesterday = 7000;
+        int stepsYesterday = 12500;
         int stepsToday = 12500;
         if (stepsToday > stepsYesterday) {
             System.out.println("Сегодня был более активен: " + stepsToday + " шагов");
         } else if (stepsYesterday > stepsToday) {
             System.out.println("Вчера был более активен: " + stepsYesterday + " шагов");
         } else {
-            System.out.println("Одинаковая активность");
+            System.out.println("Одинаковая активность: " + stepsToday + " шагов");
         }
-        int averageSteps = (stepsYesterday + stepsToday) / 2;
-        System.out.println("Среднее количество шагов: " + averageSteps);
+        int avgSteps = (stepsYesterday + stepsToday) / 2;
+        System.out.println("Среднее количество шагов: " + avgSteps);
 
-        System.out.println();
+        // 3 Задание
+        System.out.println("\n3. Проверка количества гостей\n");
 
-        // 3 ЗАДАНИЕ
-        System.out.println("3 ПРОВЕРКА КОЛИЧЕСТВА ГОСТЕЙ");
-
-        int guests = 11;
-        if (guests >= 0) {
-            if (guests == 0) {
-                System.out.println("Пока никто не записался на мероприятие!");
-            } else if (guests % 2 == 0) {
-                System.out.println("Записалось " + guests + " гостей. Можно формировать пары для конкурсов.");
-            } else {
-                System.out.println("Записалось " + guests + " гостей. Нужны индивидуальные задания.");
-            }
-        } else {
+        int guestsNum = -5;
+        if (guestsNum < 0) {
             System.out.println("Ошибка: количество гостей не может быть отрицательным.");
-        }
-
-        System.out.println();
-
-        // 4 ЗАДАНИЕ
-        System.out.println("4 ИНВЕНТАРИЗАЦИЯ");
-
-        int dbSerial = 919;
-        int actualSerial = 909;
-        if (dbSerial == actualSerial) {
-            System.out.println("[№" + dbSerial + "]: компьютер на 3-м этаже в кабинете 2");
         } else {
-            int dbHundreds = dbSerial / 100;
-            int dbTens = (dbSerial / 10) % 10;
-            int dbUnits = dbSerial % 10;
+           if (guestsNum == 0) {
+              System.out.println("Пока никто не записался на мероприятие!");
+           } else if (guestsNum % 2 == 0) {
+              System.out.println("Записалось " + guestsNum + " гостей. Можно формировать пары для конкурсов.");
+           } else {
+              System.out.println("Записалось " + guestsNum + " гостей. Нужны индивидуальные задания.");
+           }
+       }
+
+        // 4 Задание
+        System.out.println("\n4. Инвентаризация\n");
+
+        int serialFromDb = 919;
+        int actualSerial = 909;
+        if (serialFromDb == actualSerial) {
+            System.out.println("[№" + serialFromDb + "]: компьютер на 3-м этаже в кабинете 2");
+        } else {
+            int dbHundreds = serialFromDb / 100;
+            int dbTens = (serialFromDb / 10) % 10;
+            int dbUnits = serialFromDb % 10;
 
             int actualHundreds = actualSerial / 100;
             int actualTens = (actualSerial / 10) % 10;
@@ -80,48 +74,42 @@ public class IfElseStatementTheme {
 
             if (dbHundreds == actualHundreds || dbTens == actualTens || dbUnits == actualUnits) {
                 System.out.println("Нет полного совпадения:");
-                System.out.println("База данных: [№" + dbSerial + "]");
+                System.out.println("База данных: [№" + serialFromDb + "]");
                 System.out.println("Фактический: [№" + actualSerial + "]");
             } else {
                 System.out.println("[№" + actualSerial + "]: оборудование не идентифицировано");
             }
         }
 
-        System.out.println();
-
-        // 5 ЗАДАНИЕ
-        System.out.println("5 ОПРЕДЕЛЕНИЕ ПЕРВОГО СИМВОЛА НИКНЕЙМА");
+        // 5 Задание
+        System.out.println("\n5. Определение первого символа никнейма\n");
 
         String nickname = "Dmitry36";
 
         char firstChar = nickname.charAt(0);
 
         if (firstChar >= 'a' && firstChar <= 'z') {
-            System.out.println("Имя " + nickname + " начинается с маленькой буквы '" + firstChar + "'" + "(С помощью ДИАПАЗОНА КОДОВ)");
+            System.out.println("Имя " + nickname + " начинается с маленькой буквы '" + firstChar + "' (С помощью диапазона кодов)");
         } else if (firstChar >= 'A' && firstChar <= 'Z') {
-            System.out.println("Имя " + nickname + " начинается с большой буквы '" + firstChar + "'" + "(С помощью ДИАПАЗОНА КОДОВ)");
+            System.out.println("Имя " + nickname + " начинается с большой буквы '" + firstChar + "' (С помощью диапазона кодов)");
         } else if (firstChar >= '0' && firstChar <= '9') {
-            System.out.println("Имя " + nickname + " начинается с цифры '" + firstChar + "'" + "(С помощью ДИАПАЗОНА КОДОВ)");
+            System.out.println("Имя " + nickname + " начинается с цифры '" + firstChar + "' (С помощью диапазона кодов)");
         } else {
-            System.out.println("Имя " + nickname + " начинается с символа '" + firstChar + "'" + "(С помощью ДИАПАЗОНА КОДОВ)");
+            System.out.println("Имя " + nickname + " начинается с символа '" + firstChar + "' (С помощью диапазона кодов)");
         }
-
-        System.out.println();
 
         if (Character.isLowerCase(firstChar)) {
-            System.out.println("Имя " + nickname + " начинается с маленькой буквы '" + firstChar + "'" + "(С помощью Character)");
+            System.out.println("Имя " + nickname + " начинается с маленькой буквы '" + firstChar + "' (С помощью Character)");
         } else if (Character.isUpperCase(firstChar)) {
-            System.out.println("Имя " + nickname + " начинается с большой буквы '" + firstChar + "'" + "(С помощью Character)");
+            System.out.println("Имя " + nickname + " начинается с большой буквы '" + firstChar + "' (С помощью Character)");
         } else if (Character.isDigit(firstChar)) {
-            System.out.println("Имя " + nickname + " начинается с цифры '" + firstChar + "'" + "(С помощью Character)");
+            System.out.println("Имя " + nickname + " начинается с цифры '" + firstChar + "' (С помощью Character)");
         } else {
-            System.out.println("Имя " + nickname + " начинается с символа '" + firstChar + "'" + "(С помощью Character)");
+            System.out.println("Имя " + nickname + " начинается с символа '" + firstChar + "' (С помощью Character)");
         }
 
-        System.out.println();
-
-        // 6 ЗАДАНИЕ
-        System.out.println("6 ПОДСЧЕТ НАЧИСЛЕННЫХ БАНКОМ %");
+        // 6 Задание
+        System.out.println("\n6. Подсчёт начисленных банком %\n");
 
         double deposit = 321123.59;
         double interestRate;
@@ -138,11 +126,9 @@ public class IfElseStatementTheme {
         System.out.printf("Сумма вклада: %.2f руб.\n", deposit);
         System.out.printf("Начисленный процент: %.2f руб.\n", interest);
         System.out.printf("Итоговая сумма с процентами: %.2f руб.\n", total);
-        
-        System.out.println();
 
-        // 7 ЗАДАНИЕ
-        System.out.println("7 ОПРЕДЕЛЕНИЕ ОЦЕНКИ ПО ПРЕДМЕТАМ");
+        // 7 Задание
+        System.out.println("\n7. Определение оценки по предметам\n");
 
         int historyPercent = 59;
         int programmingPercent = 92;
@@ -172,17 +158,14 @@ public class IfElseStatementTheme {
         System.out.println("История: " + historyGrade);
         System.out.println("Программирование: " + programmingGrade);
 
-        double averageGrade = (historyGrade + programmingGrade) / 2.0;
+        double avgGrade = (historyGrade + programmingGrade) / 2.0;
+        double avgPercent = (historyPercent + programmingPercent) / 2.0;
 
-        double averagePercent = (historyPercent + programmingPercent) / 2.0;
+        System.out.println("Средний балл: " + avgGrade);
+        System.out.println("Средний процент: " + avgPercent + "%");
 
-        System.out.println("Средний балл: " + averageGrade);
-        System.out.println("Средний процент: " + averagePercent + "%");
-
-        System.out.println();
-
-        // 8 ЗАДАНИЕ
-        System.out.println("8 РАСЧЕТ ГОДОВОЙ ПРИБЫЛИ");
+        // 8 Задание
+        System.out.println("\n8. Расчёт годовой прибыли\n");
 
         double monthlyRevenue = 13025.233;
         double monthlyRent = 5123.018;
