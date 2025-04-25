7ad58b3b296a119ed898c134dea31154f1ed4e41
Dmitry-J89 <lblb8358@gmail.com>
2025-04-25 11:50:06 +0500

    ДЗ-2урок-3(1+1) 25.04.25

---
+++ homework/WolfTest2.java
@@ -0,0 +1,25 @@
+public class WolfTest2 {
+    public static void main(String[] args) {
+        Wolf2 wolf = new Wolf2();
+
+        wolf.setGender("Самец");
+        wolf.setName("Мудрец");
+        wolf.setWeight(40);
+        wolf.setAge(10);
+        wolf.setColor("Серый");
+
+        System.out.println("Пол: " + wolf.getGender());
+        System.out.println("Кличка: " + wolf.getName());
+        System.out.println("Вес: " + wolf.getWeight());
+        System.out.println("Возраст: " + wolf.getAge());
+        System.out.println("Окрас: " + wolf.getColor());
+
+        wolf.go();
+        wolf.sit();
+        wolf.run();
+        wolf.howl();
+        wolf.hunt();
+
+        wolf.setAge(5);
+    }
+}
